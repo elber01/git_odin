cbff63e1f1fe3de7930be8d38a1425bd5f9ae9c9
elber01 <hamilton.edwin@gmail.com>
2024-08-31 16:34:02 -0400

    Exercises 1.1 and 1.2

---
+++ OneDrive/Escritorio/fullstackopen/part1/src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+
+const Hello = (props) => {
+
+  console.log(props)
+  return (
+    <div>
+      <p>
+
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  )
+}
+
+const App = () => {
+
+  const name = 'Peter'
+  const age = 10
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+
+      <Hello name='Maya' age={26 + 10} />
+      <Hello name={name} age={age} />
+    </div>
+  )
+}
+
+//este export es necesario para que la app funcione
+export default App
